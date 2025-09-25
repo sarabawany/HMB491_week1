4757b3dda5db4e54d4459778156bfb84b8e0f692
sarabawany <sara.bawany@mail.utoronto.ca>
2025-09-25 18:14:21 -0400

    Finished week1plot

--- week1_plot.py
+++ week1_plot.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# TODO: load the dataset as pandas dataframe
+# Load the dataset as pandas dataframe
+df_teeth = pd.read_csv("mammal_teeth.csv")
 
-plt.figure(figsize=(5, 10)) # set figure size
-plt.scatter(x=df_teeth['Top incisors'],
-y=df_teeth['MAMMAL']) # set figure x, y axis
+plt.figure(figsize=(5, 10))  # set figure size
+plt.scatter(
+    x=df_teeth['Top incisors'],
+    y=df_teeth['MAMMAL']
+)  # set figure x, y axis
 plt.gca().xaxis.set_visible(False)
 
-# TODO: change the title name to include your name
-plt.title("plot for mammal_teeth dataset")
-plt.savefig("mammal_teeth_scatterplot.png", dpi=150) # save the figure
+# Change the title name to include your name
+plt.title("Plot for mammal_teeth dataset - Sara Bawany")
+
+# Save the figure
+plt.savefig("mammal_teeth_scatterplot.png", dpi=150)
